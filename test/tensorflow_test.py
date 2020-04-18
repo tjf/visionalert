d5c2b140991eaa765033efa6e9d1aca4246f4dc7
Tyler Frederick <tyler@tylerfrederick.com>
2020-04-18 18:03:21 -0400

    Clean up tensorflow_test after refactoring.

--- test/tensorflow_test.py
+++ test/tensorflow_test.py
@@ -26,21 +26,21 @@ def mock_input_image():
 
 @pytest.fixture
 def mock_interpreter(mocker):
-    mocker.patch("visionalert.tensorflow.Interpreter")
-    return tf.Interpreter.return_value
+    mocker.patch("visionalert.tensorflow.tflite.Interpreter")
+    return tf.tflite.Interpreter.return_value
 
 
-def test_calc_relative_box(mock_input_image):
+def test_calc_bounding_box(mock_input_image):
     input_box = [0.2, 0.1, 0.8, 0.9]
     expected = tf.Rectangle(64, 96, 576, 384)
-    result = tf.calc_relative_box(mock_input_image, input_box)
+    result = tf.calc_bounding_box(mock_input_image, input_box)
     assert result == expected
 
 
-def test_calc_relative_box_with_overflows(mock_input_image):
+def test_calc_bounding_box_with_overflows(mock_input_image):
     input_box = [-0.2, -0.1, 1.2, 1.2]
     expected = tf.Rectangle(1, 1, 640, 480)
-    result = tf.calc_relative_box(mock_input_image, input_box)
+    result = tf.calc_bounding_box(mock_input_image, input_box)
     assert result == expected
 
 
@@ -51,16 +51,16 @@ def test_load_labels_and_correct_error(label_file):
 
 
 def test_objectdetector_init_tensorflow(label_file, mock_interpreter):
-    tf.ObjectDetector("", label_file)
-    tf.Interpreter.assert_called_once_with(model_path="")
+    tf.create_detector("", label_file)
+    tf.tflite.Interpreter.assert_called_once_with(model_path="", experimental_delegates=None)
     mock_interpreter.allocate_tensors.assert_called_once()
 
 
 def test_objectdetector_pass_correct_size_image(
     label_file, mock_interpreter, mock_input_image
 ):
-    detector = tf.ObjectDetector("", label_file)
-    detector.detect_objects(mock_input_image)
+    detect = tf.create_detector("", label_file)
+    detect(mock_input_image)
     input_image = mock_interpreter.set_tensor.call_args[0][1]
     # Tensorflow expects a 1x300x300x3 input image shape
     assert input_image.shape == (1, 300, 300, 3)
@@ -69,18 +69,18 @@ def test_objectdetector_pass_correct_size_image(
 def test_objectdetector_parse_detected_objects(
     label_file, mock_interpreter, mock_input_image
 ):
-    detector = tf.ObjectDetector("", label_file)
+    detect = tf.create_detector("", label_file)
 
     # Mock TensorFlow response -- Bounding box, label index, confidence score
     mock_interpreter_response = (([[0.2, 0.1, 0.8, 0.9]],), ([0],), ([0.8],))
-    tf.Interpreter.return_value.get_tensor.side_effect = [
+    tf.tflite.Interpreter.return_value.get_tensor.side_effect = [
         mock_interpreter_response[0],
         mock_interpreter_response[1],
         mock_interpreter_response[2],
     ]
 
-    expected = tf.DetectedObject("cat", 0.8, tf.Rectangle(64, 96, 576, 384))
-    result = detector.detect_objects(mock_input_image)
+    expected = tf.DetectionResult("cat", 0.8, tf.Rectangle(64, 96, 576, 384))
+    result = detect(mock_input_image)
 
     mock_interpreter.invoke.assert_called_once()
     assert result[0] == expected
